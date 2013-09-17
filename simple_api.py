ed39b74b3fd5327c27322318b136a3ef528c9ef5
Aurélien Bondis <aurelien.bondis@gmail.com>
2013-09-17 13:02:25 -0400

    reflection and added get functions

--- simple_api.py
+++ simple_api.py
@@ -7,34 +7,31 @@ from sqlalchemy.orm import sessionmaker
 #from sqlalchemy.orm import relationship
 import flask.ext.restless
 
-engine = create_engine('mysql://root:root@localhost/ec', encoding='ISO-8859-1', convert_unicode=True)
+engine = create_engine(
+    'mysql://root:root@localhost/ec',
+    encoding='ISO-8859-1',
+    convert_unicode=True)
 Base = declarative_base(cls=DeferredReflection)
-Base.metadata.bind = engine
 
 
 class RecipeTypes(Base):
     __tablename__ = 'recipe_types'
-    __table_args__ = {'autoload': True}
-
+    
 
 class RecToCoi(Base):
     __tablename__ = 'rec_to_coi'
-    __table_args__ = {'autoload': True}
 
 
 class Ingredients(Base):
     __tablename__ = 'ingredients'
-    __table_args__ = {'autoload': True}
 
 
 class Steps(Base):
     __tablename__ = 'steps'
-    __table_args__ = {'autoload': True}
 
 
 class Recipes(Base):
     __tablename__ = 'recipes'
-    __table_args__ = {'autoload': True}
 
 Base.prepare(engine)
 
@@ -47,10 +44,13 @@ app.config.from_object(__name__)
 # create a Session
 Session = sessionmaker(bind=engine)
 session = Session()
-
 manager = flask.ext.restless.APIManager(app, session=session)
 
+manager.create_api(RecipeTypes, methods=['GET'])
 manager.create_api(Recipes, methods=['GET'])
+manager.create_api(Steps, methods=['GET'])
+manager.create_api(Ingredients, methods=['GET'])
+manager.create_api(RecToCoi, methods=['GET'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
